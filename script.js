3f94dada766c2ce3d29a5c3f52eeb26021621ff8
saskiabt <sskbinder@gmail.com>
2022-05-02 23:08:01 -0400

    removed set dimensions button and cleaned up html tree, moved elements from DOM to HTML structure for greater clarity in js code

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ let content= document.getElementById('content');
 let squaresContainer = document.querySelector('.squares-container')
 let buttonContainer = document.getElementById('buttonContainer')
 let squares = squaresContainer.children
-console.log(squares); 
+    console.log(squares); 
+let inputBox = document.querySelector('.input-box'); 
+let sliderBox = document.querySelector('#slider-box'); 
+let lengthInput = document.querySelector("#lengthInput")
+let inputLabel = document.getElementById('input-label'); 
+let lengthOutput = document.querySelector('.length-output')
 
 
 
@@ -45,39 +50,8 @@ function hover(element, enter, leave) {
 
 
 let xyInput = () => { 
-    let inputButton = document.querySelector('.dimensions-button'); 
-
-    inputButton.addEventListener('click', () => { 
-
-        inputButton.remove(); 
-    // create input slider with a min of 10, max 100, and interval of 10
-        let inputBox = document.createElement('div'); 
-            buttonContainer.appendChild(inputBox); 
-            inputBox.classList.add('input-box'); 
-        
-        let inputPrompt = document.createElement('div')
-            inputPrompt.classList.add('input-prompt'); 
-            inputPrompt.textContent = "Please enter desired grid dimensions:"
-            inputBox.appendChild(inputPrompt); 
-
-        let lengthInput = document.createElement('INPUT'); 
-            inputBox.appendChild(lengthInput); 
-            lengthInput.setAttribute('type', "range");
-            lengthInput.setAttribute('name', "lengthInput");
-            lengthInput.setAttribute('id', "lengthInput");  
-            lengthInput.setAttribute('min', "0");
-            lengthInput.setAttribute('max', "100"); 
-            lengthInput.setAttribute('step', "10");
-            lengthInput.setAttribute('value', "0");
-            
-        let inputLabel = document.createElement('label')
-            inputLabel.setAttribute("for", "lengthInput"); 
-
-        let lengthOutput = document.createElement('div'); 
-            lengthOutput.classList.add('length-output'); 
-            buttonContainer.appendChild(lengthOutput)
-            lengthOutput.textContent= lengthInput.value
-    
+    createSquares(60); 
+    sizeGrid(60); 
     // when a dimension is inputted, create grid square divs and create grid layout to hold them in amt inputted
         lengthInput.addEventListener('input', () => { 
             removeChildren('.gridSquare', squaresContainer);
@@ -89,7 +63,7 @@ let xyInput = () => {
 
             sizeGrid(lengthInput.value); 
          
-        }); 
+        // }); 
 
     });
         
@@ -97,19 +71,6 @@ let xyInput = () => {
 
 xyInput(); 
 
-// function startDraw() { 
-//     squaresContainer.addEventListener('dblclick', () => { 
-//         for (let i=0; i<squares.length; i++) { 
-//             squares[i].addEventListener('mouseover', () => {
-//                 squares[i].classList.add('permahover'); 
-//             }); 
-//         }
-//     });
-// }
-
-// function endDraw() { 
-
-// }
 
 function draw() { 
     squaresContainer.addEventListener('click', () => { 
@@ -130,7 +91,19 @@ draw();
 
 
 
+// function startDraw() { 
+//     squaresContainer.addEventListener('dblclick', () => { 
+//         for (let i=0; i<squares.length; i++) { 
+//             squares[i].addEventListener('mouseover', () => {
+//                 squares[i].classList.add('permahover'); 
+//             }); 
+//         }
+//     });
+// }
 
+// function endDraw() { 
+
+// }
 
 
 // next thing to do is to make it so the computer starts drawing on click, keeps drawing on hover, and stops drawing on click!
